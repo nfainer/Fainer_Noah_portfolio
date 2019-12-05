c05f6108a1bf8ada2825206a05208b971a226bd2
Your Name <n_fainer@fanshaweonline.ca>
2019-12-04 22:06:12 -0500

    blank

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     port: 8889, // 8889 for mac
     uname: "bd1bc3782ed8e8",
     pword: "03e9d5eb", // leave blank for windows
-    database: "heroku_8be51b0d42b7d10"
+    database: "heroku_8be51b0d42b7d10?"
 }
 
 module.exports = config;

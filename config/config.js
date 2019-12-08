b40d9ca15d6d2a8126f20cf1d39cf371b2f9f1b1
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 14:35:29 -0500

    databse

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
 const config = {
     host: "cleardb.net",
     // port: 8889, // 8889 for mac
-    uname: "bd1bc3782ed8e8",
-    pword: "03e9d5eb", // leave blank for windows
-    database: "heroku_8be51b0d42b7d10?"
+    uname: "b167bf62d093ad",
+    pword: "bfd41dac", // leave blank for windows
+    database: "heroku_cd94bca7242e095"
 }
 
 module.exports = config;
+
+// /Applications/MAMP/Library/bin/mysql -u b167bf62d093ad -h us-cdbr-iron-east-05.cleardb.net -p bfd41dac < /Users/noahfainer/Desktop/db_portfolio.sql

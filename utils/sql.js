be05fb891e0f312ee193194bdd875d0f3f29212c
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 14:57:29 -0500

    db

--- utils/sql.js
+++ utils/sql.js
@@ -1,12 +1,13 @@
 const config = require("../config/config");
 const sql = require('mysql');
 
-const connect = sql.createConnection({
+const connect = sql.createPool({
     host: config.host,
     port: config.port,
     user: config.uname,
     password: config.pword,
-    database: config.database
+    database: config.database,
+    connectionLimit : 10,
 });
 
 module.exports = connect;

866bf641db165621492fa97ce342faffc99a7862
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 15:33:26 -0500

    im feeling lucky

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net",
     // port: 8889, // 8889 for mac
-    uname: "b167bf62d093ad",
-    pword: "bfd41dac", // leave blank for windows
-    database: "heroku_cd94bca7242e095"
+    user: "b880ecb1a39fec",
+    password: "bec7d1ea", // leave blank for windows
+    database: "heroku_743470523a1d2ef"
 }
 
 module.exports = config;
 
-// /Applications/MAMP/Library/bin/mysql -u b167bf62d093ad -h us-cdbr-iron-east-05.cleardb.net -p bfd41dac < /Users/noahfainer/Desktop/db_portfolio.sql
+// /Applications/MAMP/Library/bin/mysql -u b880ecb1a39fec -h us-cdbr-iron-east-05.cleardb.net -p bec7d1ea < /Users/noahfainer/Desktop/db_portfolio.sql

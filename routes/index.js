20dcb3188fdeef3475b3ced98fd1251a5d000827
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 15:07:41 -0500

    DB

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
 
     // get the connection via the connection pool, and then run the query -> just one added step
     connect.getConnection((err, connection) => {
-		if (err) { return console.log(error.message); }
+		if (err) { return console.log(err); }
 
 		let query = 'SELECT * FROM tbl_home';
 

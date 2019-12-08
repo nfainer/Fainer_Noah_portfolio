8d657218b717fdc47dc6973d5c564380524ab267
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 15:06:04 -0500

    db

--- utils/sql.js
+++ utils/sql.js
@@ -1,13 +1,15 @@
-const config = require("../config/config");
-const sql = require('mysql');
+var mysql = require('mysql');
+var config = require('../config/config');
 
-const connect = sql.createPool({
-    host: config.host,
-    port: config.port,
-    user: config.uname,
-    password: config.pword,
-    database: config.database,
-    connectionLimit : 10,
+var connect = mysql.createPool({
+  host: config.host,
+  port: config.port,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+  connectionLimit : 20,
+  queueLimit : 100,
+  waitForConnections : true
 });
 
 module.exports = connect;

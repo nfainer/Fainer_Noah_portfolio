73887e50992cc61a3521320ce724f58d10ade3bb
Your Name <n_fainer@fanshaweonline.ca>
2019-12-08 14:19:16 -0500

    trying to add database

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     host: "cleardb.net",
-    port: 8889, // 8889 for mac
+    // port: 8889, // 8889 for mac
     uname: "bd1bc3782ed8e8",
     pword: "03e9d5eb", // leave blank for windows
     database: "heroku_8be51b0d42b7d10?"
